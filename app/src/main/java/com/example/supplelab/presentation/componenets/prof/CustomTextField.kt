6131d0ae7c1bbe098dbac0c4deb1590dcbf4fa77
Ahmed <ahmedbobos.rashid@gmail.com>
2025-10-03 17:30:14 +0300

    Add AlertTextField and CustomTextField components for enhanced text input functionality

---
+++ app/src/main/java/com/example/supplelab/presentation/componenets/prof/CustomTextField.kt
@@ -0,0 +1,78 @@
+package com.example.supplelab.presentation.componenets.prof
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.example.supplelab.ui.theme.BorderError
+import com.example.supplelab.ui.theme.BorderIdle
+import com.example.supplelab.ui.theme.FontSize
+import com.example.supplelab.ui.theme.SurfaceLighter
+import com.example.supplelab.ui.theme.TextPrimary
+import com.example.supplelab.util.Constants.ALPHA_DISABLED
+import com.example.supplelab.util.Constants.ALPHA_HALF
+
+@Composable
+fun CustomTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String? = null,
+    enabled: Boolean = true,
+    error: Boolean = false,
+    expanded: Boolean = false,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(
+        keyboardType = KeyboardType.Text
+    ),
+) {
+    val borderColor by animateColorAsState(
+        targetValue = if (error) BorderError else BorderIdle
+    )
+    TextField(
+        modifier = modifier
+            .fillMaxWidth()
+            .border(
+                width = 1.dp,
+                color = borderColor,
+                shape = RoundedCornerShape(size = 6.dp)
+            ),
+        enabled = enabled,
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = if (placeholder != null) {
+            {
+                Text(
+                    modifier = Modifier.alpha(ALPHA_HALF),
+                    text = placeholder,
+                    fontSize = FontSize.REGULAR
+                )
+            }
+        } else null,
+        singleLine = !expanded,
+        shape = RoundedCornerShape(size = 6.dp),
+        keyboardOptions = keyboardOptions,
+        colors = TextFieldDefaults.colors(
+            focusedTextColor = TextPrimary,
+            unfocusedTextColor = TextPrimary,
+            disabledTextColor = TextPrimary.copy(alpha = ALPHA_DISABLED),
+            focusedContainerColor = SurfaceLighter,
+            unfocusedContainerColor = SurfaceLighter,
+            disabledContainerColor = SurfaceLighter,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            errorIndicatorColor = Color.Transparent,
+        )
+    )
+}
